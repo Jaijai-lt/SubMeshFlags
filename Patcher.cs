444486172733eed9230721a49da9d242539c0dc5
Jaijai <jaijai@nijinsky>
2022-10-04 15:57:37 +0900

    プロジェクト ファイルを追加します。

---
+++ Patcher.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
+
+namespace SubMeshFlags {
+
+    public static class Patcher
+    {
+        private const string HarmonyId = "jaijai.submeshflags";
+
+        private static bool patched = false;
+
+        public static void PatchAll()
+        {
+            if (patched) return;
+
+            patched = true;
+
+            var harmony = new Harmony(HarmonyId);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        public static void UnpatchAll()
+        {
+            if (!patched) return;
+
+            var harmony = new Harmony(HarmonyId);
+            harmony.UnpatchAll(HarmonyId);
+
+            patched = false;
+        }
+    }
+
+    [HarmonyPatch]
+    public static class UserFlagSupportedPatch
+    {
+        static MethodBase TargetMethod()
+        {
+            return typeof(REEnumBitmaskSet).GetMethod("UserFlagSupported", BindingFlags.NonPublic | BindingFlags.Instance).MakeGenericMethod(typeof(int));
+        }
+
+        public static bool Prefix(Type type)
+        {
+            return (typeof(Building.Flags) == type) | (typeof(Vehicle.Flags) == type);
+        }
+
+        public static void Postfix(ref bool __result)
+        {
+            __result = true;
+        }
+    }
+}
---
+++ SubMeshFlags.cs
@@ -0,0 +1,26 @@
+﻿using CitiesHarmony.API;
+using ColossalFramework.UI;
+using ICities;
+using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace SubMeshFlags
+{
+    public class Mod : IUserMod
+    {
+        public string Name => "Sub Mesh Flags";
+        public string Description => "Allows using all sub mesh flags";
+
+        public void OnEnabled()
+        {
+            HarmonyHelper.DoOnHarmonyReady(() => Patcher.PatchAll());
+        }
+
+        public void OnDisabled()
+        {
+            if (HarmonyHelper.IsHarmonyInstalled) Patcher.UnpatchAll();
+        }
+    }
+
+}
